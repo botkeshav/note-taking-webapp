d74a2d3b01eec1ef839760ac57606a6891789546
Keshav <botkeshav@gmail.com>
2025-08-13 07:14:45 +0530

    Added loading gif for waiting time

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,10 @@ function Login  () {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [msg,setmsg] = useState("");
+    const [loading,setloading] = useState(false);
 
   const handleLogin = async (e) => {
+    setloading(true) // Making it true so, that it shows GIF just after clicking 
     e.preventDefault(); // Very very very Important
     try {
       const res = await fetch("http://localhost:3000/login", {
@@ -17,6 +19,7 @@ function Login  () {
       });
 
       const data = await res.json();
+      // setloading(data.message)
       setmsg(data.message)
 
       console.log(data); // log what backend sends
@@ -26,9 +29,10 @@ function Login  () {
 
     } catch (err) {
       console.error("Login error:", err);
+    }finally{
+      setloading(false)
     }
   };
-
     return (
     <>
         <div className="container">  
@@ -42,8 +46,14 @@ function Login  () {
             <input type="password" value={password} placeholder="Enter Password" id='password' onChange={e => setPassword(e.target.value)}/>
             
             
-            <button id = "login-btn" type = "submit">Login </button>
-
+            {/* <button id = "login-btn" type = "submit" onClick={()=> setloading(true)}>Login </button>
+            {loading  && <img src="./loadingGIF.gif" style={{ width: '50px', height: '50px', verticalAlign: 'middle' }}  alt="Ruk ja bsdk" />  } */}
+            {!loading ? ( <button id="login-btn" type="submit" onClick={handleLogin}>Login</button>) : (<img 
+              src="./loadingGIF.gif" 
+              alt="Ruk Ja bsdk" 
+              style={{ width: '100px', height: '100px' }} 
+            />
+            )}
            <hr className="login-separator" />         
             <span id="para">Don't have an account?</span>
             <button id = "reg-btn" type = "button" onClick={() =>navigate('/register')}>Register</button> 
