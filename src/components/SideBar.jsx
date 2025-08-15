8fee8df77bbd4d0b48d54382f91c3160ad27dc0e
Keshav <botkeshav@gmail.com>
2025-08-15 23:18:47 +0530

    Added Logout function and button

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,18 @@
 import '../css/SideBar.css';
+import { useNavigate } from 'react-router-dom';
 
 
 function SideBar() {
+  const navigate = useNavigate();
+
+const handleLogout = () => {
+  localStorage.removeItem("token");
+  console.log("Logged Out Sucessfully");
+  navigate('/login'); 
+};
+
+
+
   return (
     <>
     
@@ -15,11 +26,8 @@ function SideBar() {
 
     
 
-
+    <button className='logout' onClick={handleLogout} >Logout</button>
     </div>
-
-    
-    
     </>
   )
 }
