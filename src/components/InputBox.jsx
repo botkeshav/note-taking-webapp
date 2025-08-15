2e2afb0c43befb77ed062b021c0be48f2c3d1a0a
Keshav <botkeshav@gmail.com>
2025-08-16 01:29:27 +0530

    Added Function for Saving Notes in DB

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,18 +1,58 @@
 // import '../css/InputBox.css'
 import styles from '../css/InputBox.module.css'
+
+import { useState } from 'react';
+
 function InputBox() {
+  const [title,setTitle] = useState("")
+  const [body,setBody] = useState("")
+
+  const handleNotes = async(e) => {
+    e.preventDefault();
+
+    try
+    {
+      const res = await fetch("http://localhost:3000/addNotes", {
+        method: "POST",
+        headers: {
+         "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("token")
+        },
+        body: JSON.stringify({ title, content: body }),
+    });
+
+
+
+    const data = await res.json()  //here was the error
+    if ( data.success === true)
+    {
+      console.log("Notes added sucessfully")
+    }
+    else
+    {
+      console.log(data)
+    }
+
+    }
+    catch(err)
+    {
+      console.error("Error",err); 
+    }
+  }
+
   return (
     // <div>InputBox</div>
     <>
   <div className="container">
         
-       <input type="text" className= {styles.para} placeholder='Notes Title' />
+       <input type="text" className= {styles.para} value ={title} onChange={e => setTitle(e.target.value)} placeholder='Notes Title' />
       {/* <h2>Hello User!</h2> */}
-      <textarea className={styles.textarea} placeholder="Type your Notes here"></textarea>
-       <button className={styles.button}>Add</button> 
+      <textarea className={styles.textarea} value = {body}  onChange={e => setBody(e.target.value)} placeholder="Type your Notes here"></textarea>
+       <button className={styles.button} onClick={handleNotes}>Add</button> 
   </div>
     </>
   )
 }
+
 
 export default InputBox
